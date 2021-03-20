5db0aabd492de6e27d8b8782cd1c0c80b881c44a
Borja <vazquezborja@hotmail.es>
2021-03-20 16:33:59 +0100

    added new simple art and fixes some map problems

--- Assets/Code/Scripts/MapLoader.cs
+++ Assets/Code/Scripts/MapLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,27 +21,23 @@ public class MapLoader : MonoBehaviour {
     [SerializeField] private Vector2 mapSize;
     [SerializeField] private Vector2 tileCenter;
     [SerializeField] private Image defaultTexture;
-    [SerializeField] private TextMeshProUGUI selected;
-    [SerializeField] private TextMeshProUGUI mousePosText;
-    [SerializeField] private TextMeshProUGUI insideRhombusText;
-    [SerializeField] private TextMeshProUGUI rhombusInfo;
-    [SerializeField] private Image rectangle;
+    [SerializeField] private TileDebugWindow tileDebugWindow;
 
     [SerializeField] private bool drawIsometricGrid = false;
     [SerializeField] private bool drawOrthographicGrid = false;
+
+    [SerializeField] private bool activateCellDebugging = true;
     
     [SerializeField] private Transform mapParent;
     
     private List<GameObject> tilePrefabList;
 
     private const float widthOffset = 1.28f, heightOffest = 0.64f;
+    
+    
 
     void Start() {
         tilePrefabList = new List<GameObject>();
-        
-        Debug.Log(lineSegmentsIntersect(new Vector2(0,0), new Vector2(2, 1), new Vector2(4, 0), new Vector2(0, 4)));
-        Debug.Log(lineSegmentsIntersect(new Vector2(0,0), new Vector2(2, 1), new Vector2(1, 0), new Vector2(0, 2)));
-        //Debug.Log(lineSegmentsIntersect(new Vector2(), new Vector2(), new Vector2(), new Vector2()));
 
         for (var _y = 0; _y < mapSize.y; _y++) {
             for (var _x = 0; _x < mapSize.x; _x++) {
@@ -57,36 +54,35 @@ public class MapLoader : MonoBehaviour {
                 tilePrefabList.Add(_go);       
             }
         }
+        
+        if(activateCellDebugging)
+            tileDebugWindow.gameObject.SetActive(true);
     }
 
+    public void onReload() {
+        foreach (Transform child in mapParent) {
+            Destroy(child.gameObject);
+        }
+        Start();
+    }
+    
     private void Update() {
-        var _mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        
         // multiply by 100 because of the 100 pixels per unit of Unity
-        var _cellPos = new Vector2(Input.mousePosition.x / (100 * widthOffset), Input.mousePosition.y / (100 * heightOffest));
-        var _cellOff = new Vector2(Input.mousePosition.x % widthOffset, Input.mousePosition.y % heightOffest);
+        var _mousePosCentered = new Vector2(Input.mousePosition.x - Screen.width / 2f, Input.mousePosition.y - Screen.height / 2f);
+        var _cellPos = new Vector2(_mousePosCentered.x / (100 * widthOffset), _mousePosCentered.y / (100 * heightOffest));
+        tileDebugWindow.tileType.text = $"CellPos: ({_cellPos.x}, {_cellPos.y})";
+        var _finalCell = getRealCell(new Vector2(_cellPos.x, _cellPos.y), _mousePosCentered);
 
-        //var _realPos = getRealCell(_cellPos);
-        
-        // Calc in grid space
-        var _selectedCell = new Vector2((_cellPos.y - tileCenter.y) + (_cellPos.x - tileCenter.x), (_cellPos.y - tileCenter.y) - (_cellPos.x - tileCenter.x));
-        var _selectedCellGrid = toGrid((int)_selectedCell.x, (int)_selectedCell.y);
 
-        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        
-        selected.text = $"X: {(int)_cellPos.x}, Y: {(int)_cellPos.y}";
-        mousePosText.text = $"X: {_mousePos.x}, Y: {_mousePos.y}";
-        //insideRhombusText.text = $"Is inside rhombus? {getRealCell(new Vector2((int)_cellPos.x, (int)_cellPos.y))}";
-
-        rectangle.transform.localPosition = new Vector3(Input.mousePosition.x - Screen.width / 2f, Input.mousePosition.y - Screen.height / 2f);
-        getRealCell(new Vector2((int)_cellPos.x, (int)_cellPos.y));
         #if UNITY_EDITOR
-        if(drawIsometricGrid) drawGridIsometric(Color.green);
-        if(drawOrthographicGrid) drawGridOrthographic(Color.magenta);
+            if(drawIsometricGrid) drawGridIsometric(Color.green);
+            if(drawOrthographicGrid) drawGridOrthographic(Color.magenta);
         #endif
+        
+        tileDebugWindow.tilePos.text = $"CellposFixed: ({(int)_finalCell.x}, {(int)_finalCell.y})";
     }
 
-    private void getRealCell(Vector2 _cellPos) {
+    private Vector2 getRealCell(Vector2 _cellPos, Vector2 _mousePosCentered) {
         // _cellPos is the left-lower corner of the rectangle of the tile, we need to calc all four vertices of the rhombus (A, B, C, D), and then, the center (Q)
         // With that, we will calc if AB, BC, CD, DA intersects with QMosePos. If no intersection, the mouse is over the rhombus inside the rectangle, else
         //    - if AB, the mouse is over the rhombus on upper-left
@@ -94,63 +90,77 @@ public class MapLoader : MonoBehaviour {
         //    - if CD, the mouse is over the rhombus on lower-right
         //    - if DA, the mouse is over the rhombus on lower-left
 
+        var _collisionDebug = "";
+        var _sideMultiplierX = _cellPos.x < 0 ? -1 : 1;
+        var _sideMultiplierY = _cellPos.y < 0 ? -1 : 1;
+        
+        _cellPos.x = (int) _cellPos.x;
+        _cellPos.y = (int) _cellPos.y;
+        
+        var _finalCell = new Vector2(_cellPos.x, _cellPos.y);
         _cellPos.x *= widthOffset * 100;
         _cellPos.y *= heightOffest * 100;
-        
-        var A = new Vector2(_cellPos.x, _cellPos.y + heightOffest / 2f * 100);
-        var B = new Vector2(_cellPos.x + widthOffset / 2f * 100, _cellPos.y + heightOffest * 100);
-        var C = new Vector2(_cellPos.x + widthOffset * 100, _cellPos.y + heightOffest / 2f * 100);
-        var D = new Vector2(_cellPos.x + widthOffset / 2f * 100, _cellPos.y);
-        
-        var Q = new Vector2(_cellPos.x + widthOffset / 2f * 100, _cellPos.y + heightOffest / 2f * 100);
-        var P = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-        rhombusInfo.text = $"[A{A}, B{B}, C{C}, D{D}, Q{Q}, P{P}] ";
+        var A = new Vector2(_cellPos.x, _cellPos.y + heightOffest / 2f * 100 * _sideMultiplierY);
+        var B = new Vector2(_cellPos.x + widthOffset / 2f * 100 * _sideMultiplierX, _cellPos.y + heightOffest * 100 * _sideMultiplierY);
+        var C = new Vector2(_cellPos.x + widthOffset * 100 * _sideMultiplierX, _cellPos.y + heightOffest / 2f * 100 * _sideMultiplierY);
+        var D = new Vector2(_cellPos.x + widthOffset / 2f * 100 * _sideMultiplierX, _cellPos.y);
         
-        Debug.DrawLine(new Vector3((Q.x - Screen.width / 2f) / 100f, (Q.y - Screen.height / 2f) / 100f), new Vector3(
-            (P.x - Screen.width / 2f) / 100f, (P.y - Screen.height / 2f) / 100f), Color.red);
-        
-        // Rhombus
-        Debug.DrawLine(new Vector3((A.x - Screen.width / 2f) / 100f, (A.y - Screen.height / 2f) / 100f), new Vector3(
-            (B.x - Screen.width / 2f) / 100f, (B.y - Screen.height / 2f) / 100f), Color.green);
-        
-        Debug.DrawLine(new Vector3((B.x - Screen.width / 2f) / 100f, (B.y - Screen.height / 2f) / 100f), new Vector3(
-            (C.x - Screen.width / 2f) / 100f, (C.y - Screen.height / 2f) / 100f), Color.green);
-        
-        Debug.DrawLine(new Vector3((C.x - Screen.width / 2f) / 100f, (C.y - Screen.height / 2f) / 100f), new Vector3(
-            (D.x - Screen.width / 2f) / 100f, (D.y - Screen.height / 2f) / 100f), Color.green);
-        
-        Debug.DrawLine(new Vector3((D.x - Screen.width / 2f) / 100f, (D.y - Screen.height / 2f) / 100f), new Vector3(
-            (A.x - Screen.width / 2f) / 100f, (A.y - Screen.height / 2f) / 100f), Color.green);
+        var Q = new Vector2(_cellPos.x + widthOffset / 2f * 100 * _sideMultiplierX, _cellPos.y + heightOffest / 2f * 100 * _sideMultiplierY);
+        var P = new Vector2(_mousePosCentered.x, _mousePosCentered.y);
 
-        
-        // Rectangle
-        Debug.DrawLine(new Vector3(_cellPos.x, _cellPos.y), new Vector3(_cellPos.x, _cellPos.y + heightOffest), Color.yellow);
-        Debug.DrawLine(new Vector3(_cellPos.x, _cellPos.y), new Vector3(_cellPos.x, _cellPos.y + heightOffest), Color.yellow);
-        Debug.DrawLine(new Vector3(_cellPos.x, _cellPos.y), new Vector3(_cellPos.x, _cellPos.y + heightOffest), Color.yellow);
-        Debug.DrawLine(new Vector3(_cellPos.x, _cellPos.y), new Vector3(_cellPos.x, _cellPos.y + heightOffest), Color.yellow);
-        
+        // Check intersections
         if (lineSegmentsIntersect(A, B, Q, P)) {
-            rhombusInfo.text += "intersects in AB";
-            //return false;
+            // +1 on y
+            _collisionDebug = "AB";
+            correctValues(out A, out B, out C, out D, out Q, _cellPos, -widthOffset / 2f * 100, heightOffest / 2f * 100, _sideMultiplierX, _sideMultiplierY);
+            //_finalCell.y++;
         }
         
         if (lineSegmentsIntersect(B, C, Q, P)) {
-            rhombusInfo.text += "intersects in BC";
-            //return false;
+            // +1 on x
+            _collisionDebug = "BC";
+            correctValues(out A, out B, out C, out D, out Q, _cellPos, widthOffset / 2f * 100, heightOffest / 2f * 100, _sideMultiplierX, _sideMultiplierY);
+            //_finalCell.x++;
         }
         
         if (lineSegmentsIntersect(C, D, Q, P)) {
-            rhombusInfo.text += "intersects in CD";
-            //return false;
+            // -1 on x
+            _collisionDebug = "CD";
+            correctValues(out A, out B, out C, out D, out Q, _cellPos, widthOffset / 2f * 100, -heightOffest / 2f * 100, _sideMultiplierX, _sideMultiplierY);
+            //_finalCell.x--;
         }
         
         if (lineSegmentsIntersect(D, A, Q, P)) {
-            rhombusInfo.text += "intersects in DA";
-            //return false;
+            // -1 on y
+            _collisionDebug = "DA";
+            correctValues(out A, out B, out C, out D, out Q, _cellPos, -widthOffset / 2f * 100, -heightOffest / 2f * 100, _sideMultiplierX, _sideMultiplierY);
+            //_finalCell.y--;
         }
 
-        //return true;
+        if (activateCellDebugging) {
+            // Cell outline
+            Debug.DrawLine(new Vector3(A.x / 100f, A.y / 100f), new Vector3(B.x / 100f, B.y / 100f), !_collisionDebug.Contains("AB") ? Color.green : Color.red);
+            Debug.DrawLine(new Vector3(B.x / 100f, B.y / 100f), new Vector3(C.x / 100f, C.y / 100f), !_collisionDebug.Contains("BC") ? Color.green : Color.red);
+            Debug.DrawLine(new Vector3(C.x / 100f, C.y / 100f), new Vector3(D.x / 100f, D.y / 100f), !_collisionDebug.Contains("CD") ? Color.green : Color.red);
+            Debug.DrawLine(new Vector3(D.x / 100f, D.y / 100f), new Vector3(A.x / 100f, A.y / 100f), !_collisionDebug.Contains("DA") ? Color.green : Color.red);
+        
+            // Line from Q to mouse pos
+            Debug.DrawLine(new Vector3(Q.x / 100f, Q.y / 100f), new Vector3(P.x / 100f, P.y / 100f), Color.red);
+            
+            int _windowOffsetX = 96, _windowOffsetY = _windowOffsetX;
+            tileDebugWindow.gameObject.transform.localPosition = new Vector3(_mousePosCentered.x + _windowOffsetX, _mousePosCentered.y + _windowOffsetY);
+        }
+
+        return new Vector2((int)_finalCell.x, (int)_finalCell.y);
+    }
+
+    private void correctValues(out Vector2 A, out Vector2 B, out Vector2 C, out Vector2 D, out Vector2 Q, Vector2 _cellInitPos, float _xCorrection, float _yCorrection, int _xSideM, int _ySideM) {
+        A = new Vector2(_cellInitPos.x + _xCorrection * _xSideM, _cellInitPos.y + _yCorrection * _ySideM + heightOffest / 2f * 100 * _ySideM);
+        B = new Vector2(_cellInitPos.x + _xCorrection * _xSideM + widthOffset / 2f * 100 * _xSideM, _cellInitPos.y + _yCorrection * _ySideM + heightOffest * 100 * _ySideM);
+        C = new Vector2(_cellInitPos.x + _xCorrection * _xSideM + widthOffset * 100 * _xSideM, _cellInitPos.y + _yCorrection * _ySideM + heightOffest / 2f * 100 * _ySideM);
+        D = new Vector2(_cellInitPos.x + _xCorrection * _xSideM + widthOffset / 2f * 100 * _xSideM, _cellInitPos.y + _yCorrection * _ySideM);
+        Q = new Vector2(_cellInitPos.x + _xCorrection * _xSideM + widthOffset / 2f * 100 * _xSideM, _cellInitPos.y + _yCorrection * _ySideM + heightOffest / 2f * 100 * _ySideM);
     }
     
     private void drawGridOrthographic(Color _color) {
@@ -162,10 +172,10 @@ public class MapLoader : MonoBehaviour {
         var _width = _height * _cam.aspect;
         
         for (var _r = 0; _r < _rows; _r++)
-            Debug.DrawLine(new Vector3(-_width / 2f, heightOffest * _r - heightOffest * 15.5f), new Vector3(_width / 2f, heightOffest * _r - heightOffest * 15.5f), _color);
+            Debug.DrawLine(new Vector3(-_width / 2f, heightOffest * _r - heightOffest * 15f), new Vector3(_width / 2f, heightOffest * _r - heightOffest * 15f), _color);
 
         for (var _c = 0; _c < _columns; _c++) 
-            Debug.DrawLine(new Vector3(widthOffset * _c - widthOffset * 15.5f, -heightOffest * 15.5f), new Vector3(widthOffset * _c - widthOffset * 15.5f, heightOffest * 15.5f, 0), _color);
+            Debug.DrawLine(new Vector3(widthOffset * _c - widthOffset * 15f, -heightOffest * 15f), new Vector3(widthOffset * _c - widthOffset * 15f, heightOffest * 15f, 0), _color);
     }
     
     private void drawGridIsometric(Color _color) {
@@ -185,10 +195,13 @@ public class MapLoader : MonoBehaviour {
 
     private Vector2 toGrid(int _x, int _y) {
         var _tileX = (_x - _y ) * (widthOffset / 2f);
-        var _tileY = -(_x + _y) * (heightOffest / 2f);
+        var _tileY = (_x + _y) * (heightOffest / 2f);
 
+        _tileX += widthOffset / 2f;
+        _tileY += heightOffest / 2f;
+        
         _tileX += tileCenter.x * (widthOffset);
-        _tileY -= tileCenter.y * (heightOffest);
+        _tileY += -tileCenter.y * (heightOffest);
         
         return new Vector2(_tileX, _tileY);
     }
---
+++ Assets/Code/Scripts/TileDebugWindow.cs
@@ -0,0 +1,7 @@
+using TMPro;
+using UnityEngine;
+
+public class TileDebugWindow : MonoBehaviour {
+    [SerializeField] public TextMeshProUGUI tilePos;
+    [SerializeField] public TextMeshProUGUI tileType;
+}
